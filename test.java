88ae3f29014884dc865443dbf1e0babdec705e2c
Jann Horn <jannhorn@googlemail.com>
2011-06-25 23:33:15 +0200

    added support for "for name in names" (without specifying type!)

--- test.java
+++ test.java
@@ -1,17 +1,22 @@
-package thejh.prettyJava                          ;
-                                                  
-import java.io.*                                  ;
-                                                  
-public class PrettyJava                           {
-  public static int getIndentation(String line)   {
-    for (int i = 0; i < line.length(); i++)       {
-      char c = line.charAt(i)                     ;
-      if (c != ' ')                               {
-        //return something                        
-        return i                                  ;}}
-                                                  
-    return -1                                     ;}
-                                                  
-  public static void testFor()                    {
-    for (int i = 0; i < 10; i++)                  {
-      System.out.println(i)                       ;}}}
+package thejh.prettyJava                                         ;
+                                                                 
+import java.io.*                                                 ;
+                                                                 
+public class PrettyJava                                          {
+  public static int getIndentation(String line)                  {
+    for (int i = 0; i < line.length(); i++)                      {
+      char c = line.charAt(i)                                    ;
+      if (c != ' ')                                              {
+        //return something                                       
+        return i                                                 ;}}
+                                                                 
+    return -1                                                    ;}
+                                                                 
+  public static void testFor()                                   {
+    for (int i = 0; i < 10; i++)                                 {
+      System.out.println(i)                                      ;}}
+                                                                 
+  public static void foo()                                       {
+    String[] names = new String[]{"Alice", "Bob", "Evil Eve"};   ;
+    for (String name: names)                                     {
+      System.out.println(name)                                   ;}}}
